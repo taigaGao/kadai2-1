7f3f0b5eb69af17acca74632bce18c44f73639c9
taiga <gaitagaita69@gmail.com>
2020-06-13 00:08:43 +0900

    指摘事項の修正

--- kadai.js
+++ kadai.js
@@ -14,54 +14,45 @@ window.onload = function () {
         while (textArea.firstChild) {
             textArea.removeChild(textArea.firstChild );
         }
-        const numList = [];
-        const inputF = document.getElementById('fizz').value;
-        const input_fizz = Number(inputF);
-        const inputB = document.getElementById('buzz').value;
-        const input_buzz = Number(inputB);
+        let numList = [];
+        const input_fizz = Number(document.getElementById('fizz').value);
+        const input_buzz = Number(document.getElementById('buzz').value);
         if (input_fizz === 0) {
             errMessage();
+            return;
         } else if (input_buzz === 0) {
             errMessage();
-        } else {
-            if (Number.isInteger(input_fizz) && Number.isInteger(input_buzz)) {
-                for (let i = 1; i < 100; i++) {
-                    const fizzNum = input_fizz * i;
-                    if (fizzNum < 100) {
-                        if (numList.includes(fizzNum)) {
-                        } else {
-                            numList.push(fizzNum);
-                        }
-                    }
-                    const buzzNum = input_buzz * i;
-                    if (buzzNum < 100) {
-                        if (numList.includes(buzzNum)) {
-                        } else {
-                            numList.push(buzzNum);
-                        }
-                    }
-                };
-                numList.sort(
-                    function (a, b) {
-                        return (a < b ? -1 : 1);
-                    }
-                );
-                console.log(numList);
-                for (let i = 0; i < numList.length; i++) {
-                    if (numList[i] % input_fizz  === 0 && numList[i] % input_buzz === 0 ) {
-                        let text = "FizzBuzz " + numList[i];
-                        writeTest(text);
-                    } else if(numList[i] % input_fizz === 0){
-                        let text = "Fizz " + numList[i];
-                        writeTest(text);
-                    } else if(numList[i] % input_buzz === 0){
-                        let text = "Buzz " + numList[i];
-                        writeTest(text);
-                    };
+            return;
+        }
+        if (Number.isInteger(input_fizz) && Number.isInteger(input_buzz)) {
+            for (let i = 1; i < 100; i++) {
+                const fizzNum = input_fizz * i;
+                numList.push(fizzNum);
+                const buzzNum = input_buzz * i;
+                numList.push(buzzNum);
+            };
+            numList.sort(
+                function (a, b) {
+                    return (a < b ? -1 : 1);
                 }
-            } else {
-                errMessage();
+            );
+            numList = numList.filter(function (x, i, self) {
+                return self.indexOf(x) === i;
+            });
+            for (let i = 0; numList[i] < 100; i++) {
+                if (numList[i] % input_fizz  === 0 && numList[i] % input_buzz === 0 ) {
+                    let text = "FizzBuzz " + numList[i];
+                    writeTest(text);
+                } else if(numList[i] % input_fizz === 0){
+                    let text = "Fizz " + numList[i];
+                    writeTest(text);
+                } else if(numList[i] % input_buzz === 0){
+                    let text = "Buzz " + numList[i];
+                    writeTest(text);
+                };
             }
+        } else {
+            errMessage();
         }
     };
 }
